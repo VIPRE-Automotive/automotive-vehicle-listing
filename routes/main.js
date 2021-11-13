4ecec5b084005472a42e690671f825ab5c749400
Alec M <alecmattu1234@gmail.com>
2021-11-13 16:05:11 -0500

    Generate random data for search results

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 // Imports
 import express from 'express';
+import faker from 'faker';
 
 // Instantiate router component
 const router = express.Router();
@@ -11,12 +12,12 @@ const router = express.Router();
  */
 router.get('/', async (request, response) => {
   // Send default response
-  response.render('index', {config: {app_name: "Test App"}});
+  response.render('index', {config: {app_name: "Test App"}, faker});
 });
 
 router.get('/vehicles/:vehicle', async (request, response) => {
   // Send default response
-  response.render('listing', {config: {app_name: "Test App"}});  
+  response.render('listing', {config: {app_name: "Test App"}, faker});
 });
 
 // Export Router
